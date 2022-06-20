a05e8b84d478e9321b6188608129d10d50d12fcf
Jesus Martinez <jesus.martinez@axity.com>
2022-06-20 05:19:39 -0500

    Primera version

---
+++ consumer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                ClientId = "consumer1",
+                GroupId = "group1",
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnableAutoCommit = true, // default(true)
+                EnableAutoOffsetStore = true, // default (true)
+            };
+
+            ConsumerBuilder<Ignore, string> consumerBuilder = new ConsumerBuilder<Ignore, string>(config);
+
+            using (IConsumer<Ignore, string> consumer = consumerBuilder.Build())
+            {
+                consumer.Subscribe("test-topic");
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = consumer.Consume(cts.Token);
+
+                            // Procesar el mensaje
+                            Console.WriteLine($"Mensaje recibido Topic: {cr.Topic}. Partición: {cr.Partition.Value}. Key: {cr.Message.Key}. Valor: {cr.Message.Value}.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine("Error occured: {0}", e.Error.Reason);
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                }
+
+                consumer.Close();
+            }
+        }
+    }
+}
---
+++ producer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string topicName = "test-topic";
+
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                ClientId = "producer1",
+            };
+
+            ProducerBuilder<Null, string> producerBuilder = new ProducerBuilder<Null, string>(config);
+
+            using (IProducer<Null, string> producer = producerBuilder.Build())
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    var message = new Message<Null, string>
+                    {
+                        Value = $"Mensaje {i}",
+                    };
+
+                    producer.Produce(topicName, message, (deliveryReport)=>
+                    {
+                        if (!deliveryReport.Error.IsError)
+                        {
+                            Console.WriteLine($"Mensaje entregado en {deliveryReport.TopicPartitionOffset} Mensaje: {deliveryReport.Message.Value}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Error en entrega {deliveryReport.Error.Reason}");
+                        }
+                    });
+                }
+
+                producer.Flush();
+            }
+        }
+    }
+}
